1cf04a789d292fb315a1d7de571e2554c9998ad5
luisjavcr10 <ing.castillorabanal@gmail.com>
2024-12-16 16:40:11 -0500

    add html

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html","spa.html","salon.html","hombre.html","producto.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
